6523ad531b0af01b6d7f90e5f0b57f12ead792a7
CORP\s_zervos <s_zervos@wargaming.net>
2024-03-06 21:27:07 +0200

    Creating game state class

---
+++ Source/Checkers/Public/CheckersGameState.h
@@ -0,0 +1,17 @@
+// Copyright 2024 Spyridon Zervos
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "CheckersGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHECKERS_API ACheckersGameState : public AGameState
+{
+	GENERATED_BODY()
+	
+};
