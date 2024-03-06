c296e4736bcfa6f2a2c3e1912289ecfcc154b3db
CORP\s_zervos <s_zervos@wargaming.net>
2024-03-06 21:33:53 +0200

    Creating game instance

---
+++ Source/Checkers/Public/CheckersGameInstance.h
@@ -0,0 +1,17 @@
+// Copyright 2024 Spyridon Zervos
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "CheckersGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHECKERS_API UCheckersGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
