e01b1863b2efb1df763f855bb14104f092907717
CORP\s_zervos <s_zervos@wargaming.net>
2024-03-06 21:17:26 +0200

    Creating main menu game mode

---
+++ Source/Checkers/Private/CheckersGameMode.cpp
@@ -0,0 +1,4 @@
+﻿// Copyright 2024 Spyridon Zervos
+
+
+#include "Checkers/Public/CheckersGameMode.h"
---
+++ Source/Checkers/Public/CheckersGameMode.h
@@ -0,0 +1,16 @@
+﻿// Copyright 2024 Spyridon Zervos
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "CheckersGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHECKERS_API ACheckersGameMode : public AGameMode
+{
+	GENERATED_BODY()
+};
