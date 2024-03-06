0a2547cb2dbec1e6a8a46d3d5d40b904c5e6682e
CORP\s_zervos <s_zervos@wargaming.net>
2024-03-06 20:39:51 +0200

    Repo restructuring

---
+++ Source/Checkers/Checkers.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Checkers.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Checkers, "Checkers" );
