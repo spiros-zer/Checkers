b3f3feda158f9fb3a30ab5ae2091c2b346db075a
CORP\s_zervos <s_zervos@wargaming.net>
2024-03-09 18:37:54 +0200

    Adding CommonUI to expose UCommonUserWidget library in c++

--- Source/Checkers/Checkers.Build.cs
+++ Source/Checkers/Checkers.Build.cs
@@ -8,7 +8,7 @@ public class Checkers : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "CommonUI"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
