dc8e21050d680985dbb4bc43a276bdd3269ec667
CORP\s_zervos <s_zervos@wargaming.net>
2024-03-10 00:54:02 +0200

    Created common action input class

---
+++ Source/Checkers/Public/UI/CheckersActionWidget.h
@@ -0,0 +1,32 @@
+﻿// Copyright 2024 Spyridon Zervos
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonActionWidget.h"
+#include "CheckersActionWidget.generated.h"
+
+class UEnhancedInputLocalPlayerSubsystem;
+
+/**
+ * 
+ */
+UCLASS()
+class CHECKERS_API UCheckersActionWidget : public UCommonActionWidget
+{
+	GENERATED_BODY()
+
+public:
+	
+	//~ Begin UCommonActionWidget interface
+	virtual FSlateBrush GetIcon() const override;
+	//~ End of UCommonActionWidget interface
+
+	/** The Enhanced Input Action that is associated with this Common Input action. */
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	const TObjectPtr<UInputAction> AssociatedInputAction;
+
+private:
+
+	UEnhancedInputLocalPlayerSubsystem* GetEnhancedInputSubsystem() const;
+};
