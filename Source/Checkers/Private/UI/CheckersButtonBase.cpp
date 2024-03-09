9d1e54505c663fabf10f38e6e89a7d3b94171a33
CORP\s_zervos <s_zervos@wargaming.net>
2024-03-09 18:39:38 +0200

    Creating the base class for buttons

---
+++ Source/Checkers/Private/UI/CheckersButtonBase.cpp
@@ -0,0 +1,4 @@
+﻿// Copyright 2024 Spyridon Zervos
+
+
+#include "UI/CheckersButtonBase.h"
---
+++ Source/Checkers/Public/UI/CheckersButtonBase.h
@@ -0,0 +1,17 @@
+﻿// Copyright 2024 Spyridon Zervos
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonButtonBase.h"
+
+#include "CheckersButtonBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract, BlueprintType, Blueprintable)
+class CHECKERS_API UCheckersButtonBase : public UCommonButtonBase
+{
+	GENERATED_BODY()
+};
