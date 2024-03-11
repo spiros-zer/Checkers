5feed533618da794587556e1f90f0f984542de7a
CORP\s_zervos <s_zervos@wargaming.net>
2024-03-11 22:58:07 +0200

    Creating the ui manager

---
+++ Source/Checkers/Private/Subsystems/CheckersUIManagerSubsystem.cpp
@@ -0,0 +1,4 @@
+﻿// Copyright 2024 Spyridon Zervos
+
+
+#include "Subsystems/CheckersUIManagerSubsystem.h"
---
+++ Source/Checkers/Public/Subsystems/CheckersUIManagerSubsystem.h
@@ -0,0 +1,32 @@
+﻿// Copyright 2024 Spyridon Zervos
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "CheckersUIManagerSubsystem.generated.h"
+
+class UCommonActivatableWidget;
+class UCheckersScreenLayoutBase;
+
+/**
+ * 
+ */
+UCLASS()
+class CHECKERS_API UCheckersUIManagerSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, BlueprintGetter)
+	UCheckersScreenLayoutBase* GetScreenLayout() const {return ScreenLayout;}
+
+	UFUNCTION(BlueprintCallable, BlueprintSetter)
+	void SetScreenLayout(UCheckersScreenLayoutBase* InScreenLayout) {ScreenLayout = InScreenLayout;}
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintGetter = "GetScreenLayout", BlueprintSetter = "SetScreenLayout")
+	TObjectPtr<UCheckersScreenLayoutBase> ScreenLayout {};
+};
