1bfa28a9976e03507fbf0a848e2be4464b436ede
CORP\s_zervos <s_zervos@wargaming.net>
2024-03-11 22:56:57 +0200

    Creating the base screen layout

---
+++ Source/Checkers/Public/UI/CheckersScreenLayoutBase.h
@@ -0,0 +1,16 @@
+﻿// Copyright 2024 Spyridon Zervos
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonUserWidget.h"
+#include "CheckersScreenLayoutBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHECKERS_API UCheckersScreenLayoutBase : public UCommonUserWidget
+{
+	GENERATED_BODY()
+};
