92d0c2abd107c3dee19a971ed38c4adbdf9a0b77
CORP\s_zervos <s_zervos@wargaming.net>
2024-03-16 21:23:30 +0200

    Updated notification subsystem

--- Plugins/CommonNotificationSubsystem/Source/Private/NotificationType.cpp
+++ Plugins/CommonNotificationSubsystem/Source/Private/NotificationType.cpp
@@ -3,34 +3,37 @@
 
 #include "NotificationType.h"
 
-UNotificationType* UNotificationType::CreateNotificationSingle(const FText& InTitle, const FText& InContent)
+UNotificationType* UNotificationType::CreateNotificationSingle(const FText& InTitle, const FText& InContent, const FText& InActionText)
 {
 	UNotificationType* NotificationType = NewObject<UNotificationType>();
 	NotificationType->Title = InTitle;
 	NotificationType->Body = InContent;
 
-	FNotificationContext ConfirmAction;
-	ConfirmAction.Result = ENotificationAction::Affirmative;
+	FNotificationContext NotificationContext;
+	NotificationContext.Result = ENotificationAction::Affirmative;
+	NotificationContext.ActionText = InActionText;
 
-	NotificationType->ButtonsContext.Add(ConfirmAction);
+	NotificationType->ButtonsContext.Add(NotificationContext);
 
 	return NotificationType;
 }
 
-UNotificationType* UNotificationType::CreateNotificationDouble(const FText& InTitle, const FText& InContent)
+UNotificationType* UNotificationType::CreateNotificationDouble(const FText& InTitle, const FText& InContent, TArray<FText> InActionTexts)
 {
 	UNotificationType* NotificationType = NewObject<UNotificationType>();
 	NotificationType->Title = InTitle;
 	NotificationType->Body = InContent;
 
-	FNotificationContext ConfirmAction;
-	ConfirmAction.Result = ENotificationAction::Affirmative;
+	FNotificationContext NotificationContextAffirmative;
+	NotificationContextAffirmative.Result = ENotificationAction::Affirmative;
+	NotificationContextAffirmative.ActionText = InActionTexts[0];
 
-	FNotificationContext CancelAction;
-	CancelAction.Result = ENotificationAction::Cancelled;
+	FNotificationContext NotificationContextNegative;
+	NotificationContextNegative.Result = ENotificationAction::Cancelled;
+	NotificationContextNegative.ActionText = InActionTexts[1];
 
-	NotificationType->ButtonsContext.Add(ConfirmAction);
-	NotificationType->ButtonsContext.Add(CancelAction);
+	NotificationType->ButtonsContext.Add(NotificationContextAffirmative);
+	NotificationType->ButtonsContext.Add(NotificationContextNegative);
 
 	return NotificationType;
 }
--- Plugins/CommonNotificationSubsystem/Source/Public/NotificationType.h
+++ Plugins/CommonNotificationSubsystem/Source/Public/NotificationType.h
@@ -12,18 +12,23 @@ struct FNotificationContext
 {
 	GENERATED_BODY()
 	
-	/** Required: The dialog option to provide. */
+	/** The dialog option to provide. */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	ENotificationAction Result = ENotificationAction::Unknown;
 
+	/** Text that will be displayed in the button related to this action. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText ActionText;
+
 	bool operator==(const FNotificationContext& Other) const
 	{
-		return Result == Other.Result;
+		return Result == Other.Result && ActionText.EqualTo(Other.ActionText);
 	}
 };
 
 /**
- * 
+ * Describes the type of the notification. Currently supporting notification with one and two buttons - single and
+ * double. 
  */
 UCLASS()
 class COMMONNOTIFICATIONSUBSYSTEM_API UNotificationType : public UObject
@@ -32,8 +37,9 @@ class COMMONNOTIFICATIONSUBSYSTEM_API UNotificationType : public UObject
 
 public:
 	
-	static UNotificationType* CreateNotificationSingle(const FText& InTitle, const FText& InContent);
-	static UNotificationType* CreateNotificationDouble(const FText& InTitle, const FText& InContent);
+	static UNotificationType* CreateNotificationSingle(const FText& InTitle, const FText& InContent, const FText& InActionText);
+	
+	static UNotificationType* CreateNotificationDouble(const FText& InTitle, const FText& InContent, TArray<FText> InActionTexts);
 
 	/** The header of the message to display */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
