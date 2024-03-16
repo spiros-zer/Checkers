31512393c82d0c45ab0ed743b1d0a01ddc602700
CORP\s_zervos <s_zervos@wargaming.net>
2024-03-16 18:43:28 +0200

    Creating notification subsystem

---
+++ Source/Checkers/Private/Subsystems/CheckersNotificationSubsystem.cpp
@@ -0,0 +1,11 @@
+﻿// Copyright 2024 Spyridon Zervos
+
+
+#include "Subsystems/CheckersNotificationSubsystem.h"
+
+void UCheckersNotificationSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+	
+	//NotificationClassPtr = NotificationClassPtrClass.LoadSynchronous();
+}
---
+++ Source/Checkers/Public/Subsystems/CheckersNotificationSubsystem.h
@@ -0,0 +1,20 @@
+﻿// Copyright 2024 Spyridon Zervos
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/LocalPlayerSubsystem.h"
+#include "CheckersNotificationSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHECKERS_API UCheckersNotificationSubsystem : public ULocalPlayerSubsystem
+{
+	GENERATED_BODY()
+
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+
+	
+};
